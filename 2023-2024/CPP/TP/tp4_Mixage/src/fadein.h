ec7a3fb05a8f74a16f451ed58a1ce864163e4328
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2024-05-30 17:18:14 +0200

    panning

---
+++ 2023-2024/CPP/TP/tp4_Mixage/src/fadein.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "filtre_base.h"
+
+class fadein : public filtre_base
+{protected:
+	float i=0;
+	float s;
+	float e;
+
+	virtual double getmult() {
+		return i <= e ? (i) / (e - s) : 1;
+	}
+
+public:
+	fadein(float start, float end) : filtre_base(1, 1), s(start), e(end) {}
+
+
+	void calculer() override {
+		if (yaDesEchantillons()) {
+			getSortie(0)->inserer(getmult() * getEntree(0)->extraire());
+
+			i++;
+		}
+	}
+
+};
---
+++ 2023-2024/CPP/TP/tp4_Mixage/src/panning.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+
+#include "filtre_base.h"
+
+class panning : public filtre_base
+{
+
+public:
+	panning() : filtre_base(3, 2) {}
+
+	void calculer() override {
+		if( yaDesEchantillons())
+		{
+			double pan = getEntree(0)->extraire();
+
+			double pan_left = 0.5 * (1.0 - pan);
+			double pan_right = 0.5 * (1.0 + pan);
+
+			double left = getEntree(1)->extraire() * pan_left;
+			double right = getEntree(2)->extraire() * pan_right;
+
+			getSortie(0)->inserer(left);
+			getSortie(1)->inserer(right);
+		}
+	}
+
+	
+
+};
